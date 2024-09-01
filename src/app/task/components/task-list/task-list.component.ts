2ebabab3db38319d6054b807cc91aed9ca33d328
Mohit <mksagar111@gmail.com>
2024-09-01 20:49:34 +0530

    delete task functionality added

--- src/app/task/components/task-list/task-list.component.ts
+++ src/app/task/components/task-list/task-list.component.ts
@@ -1,9 +1,9 @@
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
-import { TaskService } from '../../services/task.service';
 import { ITask, ITaskState, } from '../../models/task.interfaces';
 import { MatTableDataSource } from '@angular/material/table';
 import { Store } from '@ngrx/store';
 import { MatPaginator } from '@angular/material/paginator';
+import { deleteTask } from '../../store/task.actions';
 
 @Component({
   selector: 'app-task-list',
@@ -19,8 +19,9 @@ export class TaskListComponent implements OnInit, AfterViewInit {
   taskTableDataSource = new MatTableDataSource<ITask>([]);
 
   selectedTask: ITask;
+  deleteTaskId: string
 
-  constructor(private _taskService: TaskService, private _store: Store<ITaskState>) { }
+  constructor(private _store: Store<ITaskState>) { }
 
   ngOnInit() {
     this._store.select('tasks').subscribe((tasks) => {
@@ -32,8 +33,13 @@ export class TaskListComponent implements OnInit, AfterViewInit {
     this.taskTableDataSource.paginator = this.taskTablePaginator
   }
 
-  deleteTask(taskId: string) {
-    this._taskService.deleteTask(taskId);
+  setDeleteTaskId(task: ITask) {
+    this.deleteTaskId = task.id
+  }
+
+  deleteTask() {
+    this._store.dispatch(deleteTask({ taskId: this.deleteTaskId }))
+    this.deleteTaskId = null
   }
 
 
